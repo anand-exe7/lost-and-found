ff5ac7c34cf5f726967b3b6da5a37d1e6473a1d1
anand-exe7 <anandsiva0317@gmail.com>
2025-09-26 19:15:27 +0530

    /

---
+++ lost-and-found/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='navbar'>
+        <div className='nav-left'>
+            <h2>Campus lost and found</h2>
+            <img src=''></img>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
