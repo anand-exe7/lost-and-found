78041276765df3252e2cffe67c931c6c18117688
anand-exe7 <anandsiva0317@gmail.com>
2025-09-28 12:02:30 +0530

    /

--- lost-and-found/src/Components/Hero.jsx
+++ lost-and-found/src/Components/Hero.jsx
@@ -45,17 +45,38 @@ const Hero = () => {
        <p className='workp'>How It Works</p>
       <div className='cards'>
         <div className='card1'>
-          <h3>Create An Account</h3>
+          <h2>1️⃣</h2>
+          <h3>  Create An Account</h3>
            <p>Sign up with your college email address for instant verification. Your student ID ensures you're connected only with verified campus community members.</p>
         </div>
         <div className='card2'>
+          <h2>2️⃣</h2>
           <h3>Report Lost or Found</h3>
         <p>Upload photos, add descriptions, and mark the exact location on our interactive campus map. Include details like time, date, and any distinguishing features.</p>
         </div>
         
         <div className='card3'>
-          <h3>Safe Exchange</h3>
-           <p>Arrange meetups at designated safe zones on campus like the library, student center, or security office. Both parties confirm the successful return.</p>
+          <h2>3️⃣</h2>
+          <h3>Browse Items</h3>
+           <p>All reported items appear in a card-based layout. Students can scroll through and quickly check if their item has been reported.</p>
+        </div>
+
+         <div className='card4'>
+          <h2>4️⃣</h2>
+          <h3>Search & Filter</h3>
+           <p>The platform includes a search option where users can enter item names or keywords to quickly locate posts. Filters such as category or date can also be applied, helping students narrow down results.</p>
+        </div>
+
+         <div className='card5'>
+          <h2>5️⃣</h2>
+          <h3>Connect with Reporter</h3>
+           <p>When someone finds an item that matches a report, they can easily reach out to the original poster. Ensuring that the right owner and finder can connect directly.</p>
+        </div>
+
+         <div className='card6'>
+          <h2>6️⃣</h2>
+          <h3>Resolve & Close Case</h3>
+           <p>Once an item is successfully returned, the post can be marked as resolved. This helps avoid duplicate claims and keeps the platform organized.</p>
         </div>
         
       </div>
